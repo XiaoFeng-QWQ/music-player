ef8b145833e97e73e32df7e0587560aa3275a535
MSCMDD <MSCAoMe@outlook.com>
2024-02-05 15:27:40 +0800

    [Opt] Improved code readability

--- init.js
+++ init.js
@@ -1,84 +1,85 @@
-var musicPlayerContainerInit = "";
-musicPlayerContainerInit += "<link rel=\"stylesheet\" href=\"https:\/\/cdn.bootcdn.net\/ajax\/libs\/mdui\/1.0.2\/css\/mdui.min.css\">";
-musicPlayerContainerInit += "<link rel=\"stylesheet\" href=\"https:\/\/cdn.staticfile.org\/aplayer\/1.10.1\/APlayer.min.css\">";
-musicPlayerContainerInit += "";
-musicPlayerContainerInit += "<div id=\"musicPlayerSettingsContent\">";
-musicPlayerContainerInit += "<style>";
-musicPlayerContainerInit += "#musicPlayerSettings {";
-musicPlayerContainerInit += "	border-radius: 10px;";
-musicPlayerContainerInit += "}";
-musicPlayerContainerInit += "";
-musicPlayerContainerInit += "#musicPlayerSettings .mdui-switch {";
-musicPlayerContainerInit += "	height: 14.3;";
-musicPlayerContainerInit += "}";
-musicPlayerContainerInit += "";
-musicPlayerContainerInit += "#musicPlayerSettings button {";
-musicPlayerContainerInit += "	margin: 0.4rem;";
-musicPlayerContainerInit += "}";
-musicPlayerContainerInit += "";
-musicPlayerContainerInit += "#musicPlayerSettings .mdui-icon {";
-musicPlayerContainerInit += "	margin-right: 5px;";
-musicPlayerContainerInit += "}";
-musicPlayerContainerInit += "<\/style>";
-musicPlayerContainerInit += "<div id=\"musicPlayerSettings\" class=\"mdui-dialog\">";
-musicPlayerContainerInit += "<div class=\"mdui-dialog-title mdui-color-grey-900\"> <i class=\"mdui-icon material-icons\">queue_music<\/i>";
-musicPlayerContainerInit += "	音乐播放器设置";
-musicPlayerContainerInit += "<\/div>";
-musicPlayerContainerInit += "<div class=\"musicPlayerSettingsContent mdui-dialog-content\">";
-musicPlayerContainerInit += "	<ul class=\"mdui-list\">";
-musicPlayerContainerInit += "		<p id=\"debug\"><\/p>";
-musicPlayerContainerInit += "		<li class=\"mdui-list-item\">";
-musicPlayerContainerInit += "			<i class=\"mdui-list-item-icon mdui-icon material-icons\">music_note<\/i>";
-musicPlayerContainerInit += "			<div class=\"mdui-list-item-title\">音乐播放器开关<\/div>";
-musicPlayerContainerInit += "			<div class=\"mdui-list-item-content\">";
-musicPlayerContainerInit += "				<label class=\"mdui-switch\">";
-musicPlayerContainerInit += "					<input class=\"music-playlist-switch\" name=\"music-playlist-switch\" type=\"checkbox\">";
-musicPlayerContainerInit += "					<i class=\"mdui-switch-icon\"><\/i>";
-musicPlayerContainerInit += "				<\/label>";
-musicPlayerContainerInit += "			<\/div>";
-musicPlayerContainerInit += "		<\/li>";
-musicPlayerContainerInit += "		<li class=\"mdui-list-item\">";
-musicPlayerContainerInit += "			<i class=\"mdui-list-item-icon mdui-icon material-icons\">play_arrow<\/i>";
-musicPlayerContainerInit += "			<div class=\"mdui-list-item-title\">自动播放开关<\/div>";
-musicPlayerContainerInit += "			<div class=\"mdui-list-item-content\">";
-musicPlayerContainerInit += "				<label class=\"mdui-switch\">";
-musicPlayerContainerInit += "					<input class=\"music-playlist-autoplay\" name=\"music-playlist-autoplay\"";
-musicPlayerContainerInit += "						type=\"checkbox\">";
-musicPlayerContainerInit += "					<i class=\"mdui-switch-icon\"><\/i>";
-musicPlayerContainerInit += "				<\/label>";
-musicPlayerContainerInit += "			<\/div>";
-musicPlayerContainerInit += "		<\/li>";
-musicPlayerContainerInit += "		<li class=\"mdui-list-item\"> <i";
-musicPlayerContainerInit += "				class=\"mdui-list-item-icon mdui-icon material-icons\">wb_cloudy<\/i>";
-musicPlayerContainerInit += "			<div class=\"mdui-list-item-title\">音乐平台<\/div>";
-musicPlayerContainerInit += "			<div class=\"mdui-list-item-content\"> <select class=\"mdui-select musicServer\"";
-musicPlayerContainerInit += "					name=\"musicServer\">";
-musicPlayerContainerInit += "					<option value=\"netease\">网易云音乐（默认）<\/option>";
-musicPlayerContainerInit += "					<option value=\"tencent\">QQ音乐<\/option>";
-musicPlayerContainerInit += "					<option value=\"kugou\">酷狗音乐<\/option>";
-musicPlayerContainerInit += "					<option value=\"xiami\">虾米音乐<\/option>";
-musicPlayerContainerInit += "					<option value=\"baidu\">百度音乐<\/option>";
-musicPlayerContainerInit += "				<\/select> <i class=\"mdui-select-icon\"><\/i> <\/div>";
-musicPlayerContainerInit += "		<\/li>";
-musicPlayerContainerInit += "		<li class=\"mdui-list-item\"> <i class=\"mdui-list-item-icon mdui-icon material-icons\">vpn_key<\/i>";
-musicPlayerContainerInit += "			<div class=\"mdui-list-item-title\">音乐歌单ID<\/div>";
-musicPlayerContainerInit += "			<div class=\"mdui-list-item-content music-playlist-id-input\"> <input";
-musicPlayerContainerInit += "					class=\"mdui-textfield-input musicPlaylistId\" name=\"musicPlaylistId\" type=\"number\">";
-musicPlayerContainerInit += "			<\/div>";
-musicPlayerContainerInit += "		<\/li>";
-musicPlayerContainerInit += "	<\/ul>";
-musicPlayerContainerInit += "<\/div>";
-musicPlayerContainerInit += "<div class=\"mdui-dialog-actions mdui-color-grey-100\"> <button";
-musicPlayerContainerInit += "		class=\"mdui-btn mdui-ripple mdui-text-color-black-text\" mdui-dialog-confirm";
-musicPlayerContainerInit += "		onclick=\"musicPlayerSaveSettings()\"> <i class=\"mdui-icon material-icons\">done_all<\/i> 保存配置并生效";
-musicPlayerContainerInit += "	<\/button>";
-musicPlayerContainerInit += "	<button class=\"mdui-btn mdui-ripple mdui-text-color-black-text\" mdui-dialog-cancel";
-musicPlayerContainerInit += "		onclick=\"musicPlayerReadConfiguration()\"> <i class=\"mdui-icon material-icons\">cancel<\/i> 取消";
-musicPlayerContainerInit += "	<\/button>";
-musicPlayerContainerInit += "<\/div>";
-musicPlayerContainerInit += "<\/div>";
-musicPlayerContainerInit += "<\/div>";
-musicPlayerContainerInit += "<div id=\"meting-js\"><\/div>";
+var musicPlayerContainerInit = `
+<link rel="stylesheet" href="https://cdn.bootcdn.net/ajax/libs/mdui/1.0.2/css/mdui.min.css">
+<link rel="stylesheet" href="https://cdn.staticfile.org/aplayer/1.10.1/APlayer.min.css">
+
+<div id="musicPlayerSettingsContent">
+<style>
+#musicPlayerSettings {
+	border-radius: 10px;
+}
+
+#musicPlayerSettings .mdui-switch {
+	height: 14.3;
+}
+
+#musicPlayerSettings button {
+	margin: 0.4rem;
+}
+
+#musicPlayerSettings .mdui-icon {
+	margin-right: 5px;
+}
+</style>
+<div id="musicPlayerSettings" class="mdui-dialog">
+<div class="mdui-dialog-title mdui-color-grey-900"> <i class="mdui-icon material-icons">queue_music</i>
+	音乐播放器设置
+</div>
+<div class="musicPlayerSettingsContent mdui-dialog-content">
+	<ul class="mdui-list">
+		<p id="debug"></p>
+		<li class="mdui-list-item">
+			<i class="mdui-list-item-icon mdui-icon material-icons">music_note</i>
+			<div class="mdui-list-item-title">音乐播放器开关</div>
+			<div class="mdui-list-item-content">
+				<label class="mdui-switch">
+					<input class="music-playlist-switch" name="music-playlist-switch" type="checkbox">
+					<i class="mdui-switch-icon"></i>
+				</label>
+			</div>
+		</li>
+		<li class="mdui-list-item">
+			<i class="mdui-list-item-icon mdui-icon material-icons">play_arrow</i>
+			<div class="mdui-list-item-title">自动播放开关</div>
+			<div class="mdui-list-item-content">
+				<label class="mdui-switch">
+					<input class="music-playlist-autoplay" name="music-playlist-autoplay"
+						type="checkbox">
+					<i class="mdui-switch-icon"></i>
+				</label>
+			</div>
+		</li>
+		<li class="mdui-list-item"> <i
+				class="mdui-list-item-icon mdui-icon material-icons">wb_cloudy</i>
+			<div class="mdui-list-item-title">音乐平台</div>
+			<div class="mdui-list-item-content"> <select class="mdui-select musicServer"
+					name="musicServer">
+					<option value="netease">网易云音乐（默认）</option>
+					<option value="tencent">QQ音乐</option>
+					<option value="kugou">酷狗音乐</option>
+					<option value="xiami">虾米音乐</option>
+					<option value="baidu">百度音乐</option>
+				</select> <i class="mdui-select-icon"></i> </div>
+		</li>
+		<li class="mdui-list-item"> <i class="mdui-list-item-icon mdui-icon material-icons">vpn_key</i>
+			<div class="mdui-list-item-title">音乐歌单ID</div>
+			<div class="mdui-list-item-content music-playlist-id-input"> <input
+					class="mdui-textfield-input musicPlaylistId" name="musicPlaylistId" type="number">
+			</div>
+		</li>
+	</ul>
+</div>
+<div class="mdui-dialog-actions mdui-color-grey-100"> <button
+		class="mdui-btn mdui-ripple mdui-text-color-black-text" mdui-dialog-confirm
+		onclick="musicPlayerSaveSettings()"> <i class="mdui-icon material-icons">done_all</i> 保存配置并生效
+	</button>
+	<button class="mdui-btn mdui-ripple mdui-text-color-black-text" mdui-dialog-cancel
+		onclick="musicPlayerReadConfiguration()"> <i class="mdui-icon material-icons">cancel</i> 取消
+    </button>
+</div>
+</div>
+</div>
+<div id="meting-js"></div>
+`;
 
 //获取正确的相对（绝对）路径
 var scriptElement = document.currentScript;
